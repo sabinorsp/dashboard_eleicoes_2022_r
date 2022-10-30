454680a7814365875d657b1ba2a4c401679a1887
Sabino <sabinorsp@gmail.com>
2022-10-30 17:16:46 -0400

    att3

--- functions/plot_result_time.R
+++ functions/plot_result_time.R
@@ -1,5 +1,5 @@
 plot_result_time <- function(df){
-  data <- read_csv('data/results_time.csv', col_names = F)
+  data <- read_csv('data/results_time.csv', show_col_types = FALSE, col_names = F)
   cand_1 <- filter(data, data$X3 == 'LULA')
   cand_2 <- filter(data, data$X3 =='JAIR BOLSONARO')
   
--- server.R
+++ server.R
@@ -2,22 +2,22 @@ server <- function(input, output, session) {
   
   values <- reactiveValues(df = data.frame())
   
-  observeEvent(reactiveTimer(360000)(), {
+  observeEvent(reactiveTimer(200000)(), {
   
     values$df <- isolate({
-      values$df <- read_csv('data/df_geral.csv')
+      values$df <- read_csv('data/df_geral.csv', show_col_types = FALSE)
     })
     
     values$df1 <- isolate({
-      values$df1 <- read_csv('data/df_primeiro.csv')
+      values$df1 <- read_csv('data/df_primeiro.csv', show_col_types = FALSE)
     })
     
     values$df2 <- isolate({
-      values$df2 <- read_csv('data/df_segundo.csv')
+      values$df2 <- read_csv('data/df_segundo.csv', show_col_types = FALSE)
     })
     
     values$df3 <- isolate({
-      values$df3 <- read_csv('data/results_time.csv')
+      values$df3 <- read_csv('data/results_time.csv', show_col_types = FALSE)
     })
     
   })
